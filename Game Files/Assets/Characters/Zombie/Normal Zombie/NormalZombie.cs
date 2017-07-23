0d80f4ed7d1807fd6c962f4b615eba5e73629f2b
Kodeman010 <kacper97pl@tlen.pl>
2017-07-23 11:19:27 +0200

    AI

---
+++ Game Files/Assets/Characters/Zombie/Normal Zombie/NormalZombie.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalZombie : MonoBehaviour {
+
+    private CharacterController thePlayer;
+    public float speed;
+    private Rigidbody2D rb;
+
+    public bool canGoUp;
+
+    private float vertical_velocity;
+    private float save_gravity;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        thePlayer = FindObjectOfType<CharacterController>();
+
+        save_gravity = rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (thePlayer.transform.position.x > rb.transform.position.x)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (canGoUp && thePlayer.transform.position.y > rb.transform.position.y)
+        {
+            rb.gravityScale = 0f;
+
+            vertical_velocity = speed / 2;
+            rb.velocity = new Vector2(0, vertical_velocity);
+        }
+        else if (canGoUp && thePlayer.transform.position.y < rb.transform.position.y)
+        {
+            rb.gravityScale = 0f;
+
+            vertical_velocity = speed / 2;
+            rb.velocity = new Vector2(0, -vertical_velocity); //rb.velocity.x,
+        }
+        if (!canGoUp || thePlayer.transform.position.y == rb.transform.position.y )
+            rb.gravityScale = save_gravity;
+
+    }
+}
--- Game Files/Assets/_Scripts/GoingUp.cs
+++ Game Files/Assets/_Scripts/GoingUp.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GoingUp : MonoBehaviour {
 
     private CharacterController thePlayer;
+    private NormalZombie theZombie;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         thePlayer = FindObjectOfType<CharacterController>();
+        theZombie = FindObjectOfType<NormalZombie>();
 	}
 	
 	void OnTriggerEnter2D(Collider2D other)
@@ -17,6 +19,10 @@ public class GoingUp : MonoBehaviour {
         {
             thePlayer.canGoUp = true;
         }
+        else if (other.tag == "Enemy")
+        {
+            theZombie.canGoUp = true;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -24,6 +30,10 @@ public class GoingUp : MonoBehaviour {
         if (other.name == "Player") // zombie tez tu musza byc, moze wywalic warunek calkiem
         {
             thePlayer.canGoUp = false;
+        }
+        else if (other.tag == "Enemy")
+        {
+            theZombie.canGoUp = false;
         }
     }
 }
